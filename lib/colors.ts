864cdf6cb4d3c026c124c4f70f0b102867b38195
chien1510i68 <72677602+chien1510i68@users.noreply.github.com>
2025-12-06 18:22:25 +0700

    update

--- lib/colors.ts
+++ lib/colors.ts
@@ -25,29 +25,7 @@ export const COLORS = {
 
   // Neutral Colors
   neutral: {
-<<<<<<< HEAD
-  white: {
-    DEFAULT: 'white'
-  },
-  black: {
-    DEFAULT: 'black'
-  },
-  gray: {
-    DEFAULT: 'gray-500',
-    50: 'gray-50',
-    100: 'gray-100',
-    200: 'gray-200',
-    300: 'gray-300',
-    400: 'gray-400',
-    500: 'gray-500',
-    600: 'gray-600',
-    700: 'gray-700',
-    800: 'gray-800',
-    900: 'gray-900',
-  }
-},
-  
-=======
+ 
     white: {
       DEFAULT: 'white'
     },
